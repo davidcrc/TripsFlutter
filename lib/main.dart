87828c4c635173bd5b98c49a0628c90002c2e268
Devid <david_dcrcr@hotmail.com>
2019-12-23 16:18:36 -0500

    Aprendimos sobre Tree Think, y utilizamos Stack y ListView para mostrar en la interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:platzi_flutter_app/description_place.dart';
 import 'package:platzi_flutter_app/review_list.dart';
+import 'package:platzi_flutter_app/gradient_back.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  String desciptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  String desciptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\n It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
   // This widget is the root of your application.
   @override
@@ -27,15 +28,29 @@ class MyApp extends StatelessWidget {
       ),
       //home: MyHomePage(title: 'Flutter Demo Home Page'),
       home: Scaffold(
+        /*
         appBar: AppBar(
           title: Text("HOLA MUDO"),
         ),
+        */
 
         // TODO: Para traer el widget creado
         // body: new DescriptionPlace("Duwili Ella", 3 ,desciptionDummy),
 
-        // NOTE:
-        body: ReviewList(),
+        // NOTE: Colocamos un estack para colocar un objeto sobre otro
+        body: Stack(
+          children: <Widget>[
+            // NOTE: Exactamente como lo muestra ListView?: en columna siempre?
+            ListView(
+              children: <Widget>[
+                  DescriptionPlace("Bahamas", 4, desciptionDummy),
+                  ReviewList(),
+              ],
+            ),
+            //
+            GradientBack(),
+          ],
+        ),
         /*body: Center(
           child: Text("HOLUuu"),
         ),
